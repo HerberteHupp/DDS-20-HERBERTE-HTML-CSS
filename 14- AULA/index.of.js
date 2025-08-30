cd08eb7a6ccdfac62a6fa243528cb9bfd49ce8fc
HERBERTE OLIVEIRA HUPP <h.hupp@labcetec.net>
2025-08-29 21:39:37 -0300

    index.of

---
+++ 14- AULA/index.of.js
@@ -0,0 +1,14 @@
+
+// funcão index.of -
+// Localizar a posição de um elemento dentro de uma string ou array.
+//Verificar se um elemento existe numa coleção.
+//Determinar o índice de ocorrência mais cedo de um item.
+     
+
+
+  
+
+
+    var frutas = ["maçã", "limao", "laranja"];
+    console.log(frutas.indexOf("laranja")); // Retorna: 1 (a palavra "banana" está na posição 1)
+    console.log(frutas.indexOf("uva")); // Retorna: -1 (a palavra "uva" não está no array)
